5dafa81e3a34c8542ddf7e0ac010665a2182791b
WonderCsabo <kozakcsabi@gmail.com>
2012-12-27 13:33:45 +0100

    par memory leak javitva

--- Tank/main.cpp
+++ Tank/main.cpp
@@ -15,16 +15,16 @@
 
 Client* startgui()
 {
-	StartGui* gui = new StartGui();
-	return gui->getClient();
+    StartGui gui;
+    return gui.getClient();
 }
 
 void addRandomBarrels(AbstractView* v)
-{	
-	for(int i = 0; i< 50 ;i++)
-	{
-		v->addBarrel(std::rand()%700, std::rand()%700, (std::rand()%20+90)/100, (std::rand()%20+90)/100);
-	}
+{
+    for(int i = 0; i< 50 ;i++)
+    {
+        v->addBarrel(std::rand()%700, std::rand()%700, (std::rand()%20+90)/100, (std::rand()%20+90)/100);
+    }
 }
 
 #if defined WINDOWS
@@ -33,62 +33,64 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine
 int main()
 #endif
 {
-	Client* client = startgui();
-	if (client==NULL)
-		return 0;
+    Client* client = startgui();
+    if (client==NULL)
+        return 0;
 
-	sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode(700,700), "Tank Battle!", sf::Style::Close);
-	AbstractView* view = new View(window,true);//a második paramért a debugolás kiírásához van, alapértelmezetten hamis
-	std::srand((unsigned)std::time(0)); 
-	addRandomBarrels(view);
+    sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode(700,700), "Tank Battle!", sf::Style::Close);
+    AbstractView* view = new View(window,true);//a második paramért a debugolás kiírásához van, alapértelmezetten hamis
+    std::srand((unsigned)std::time(0));
+    addRandomBarrels(view);
 
-	bool run = true;
+    bool run = true;
 
-	sf::Thread thread(std::bind(&recieveFromClient, view, client, run));
-	thread.launch();
-	
-	std::string console = "";
-	sf::Text consoleText;
-	consoleText.setCharacterSize(10);
-	consoleText.setColor(sf::Color(255,255,255));
-	sf::Vector2f pos(10, 10);
-	consoleText.setPosition(pos);
-	bool writeToConsole = false;
-	
-	while (window->isOpen())
-	{
-		try 
-		{
-			sf::Event event;
-			while (window->pollEvent(event))
-			{
-				if (event.type == sf::Event::Closed)
-				{
-					run = false;
-					thread.terminate();
-					client->shutDown();
-					window->close();
-				}
+    sf::Thread thread(std::bind(&recieveFromClient, view, client, run));
+    thread.launch();
 
-				client->sendEventMessage(event);
-				wrt(client, window, consoleText, console, event, writeToConsole);
-				
-			}
-			view->drawEverything();
-			window->draw(consoleText);
-			window->display();
-			sf::sleep(sf::milliseconds(10));
-		}
-		catch(std::exception e)
-		{
-			e.what();
-		}
-	}
-	
-	if (client!=NULL) {
-		client->shutDown();
-		delete client;
-	}
+    std::string console = "";
+    sf::Text consoleText;
+    consoleText.setCharacterSize(10);
+    consoleText.setColor(sf::Color(255,255,255));
+    sf::Vector2f pos(10, 10);
+    consoleText.setPosition(pos);
+    bool writeToConsole = false;
+
+    while (window->isOpen())
+    {
+        try
+        {
+            sf::Event event;
+            while (window->pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                {
+                    run = false;
+                    thread.terminate();
+                    client->shutDown();
+                    window->close();
+                }
+
+                client->sendEventMessage(event);
+                wrt(client, window, consoleText, console, event, writeToConsole);
+
+            }
+            view->drawEverything();
+            window->draw(consoleText);
+            window->display();
+            sf::sleep(sf::milliseconds(10));
+        }
+        catch(std::exception e)
+        {
+            e.what();
+        }
+    }
+
+    if (client!=NULL) {
+        client->shutDown();
+        delete client;
+    }
+
+    delete window;
 
     return 0;
 }
