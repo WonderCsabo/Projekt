f97683d84d251900021410811f1d988d7539b80f
olomhuszar <olomhuszar@gmail.com>
2012-12-30 00:05:27 +0100

    CommonTankInfo removed
    Az előző pushnál átraktam a Controllerbe, lemaradt a kitörlés.

--- Tank/Graphics/CommonTankInfo.cpp
+++
@@ -1,4 +0,0 @@
-#include "CommonTankInfo.h"
-CommonTankInfo::CommonTankInfo(unsigned short t):teamValue(t){}
-unsigned short CommonTankInfo::team(){return teamValue;}
-CommonTankInfo::~CommonTankInfo(void){}
